104fde3968e499e78bec33e9e20958cf355d903c
Zahid Sheikh <48240960+XodeXiphoidXahid@users.noreply.github.com>
2022-03-30 16:40:42 +0600

    show image using SVG

---
+++ Draw-Rectangle/Draw-Rectangle/Models/RectangleProperties.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Draw_Rectangle.Models
+{
+    public class RectangleProperties
+    {
+        public string Length { get; set; }
+        public string Width { get; set; }
+
+        public RectangleProperties()
+        {
+
+        }
+
+        internal void LoadPropertiesValue()
+        {
+            var jsonFileInfo = Path.Combine(Directory.GetCurrentDirectory(), "RectangleValues.json");
+            dynamic jsonFile = JsonConvert.DeserializeObject(File.ReadAllText(jsonFileInfo));
+
+            Length = jsonFile.Length;
+            Width = jsonFile.Width;
+        }
+    }
+}
