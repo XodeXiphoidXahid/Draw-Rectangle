9f55dc6fb3a2bbd684c2e5ca27072071bf454439
Zahid Sheikh <48240960+XodeXiphoidXahid@users.noreply.github.com>
2022-04-03 17:26:11 +0600

    post method works perfectly

--- Draw-Rectangle/Draw-Rectangle/Models/RectangleProperties.cs
+++ Draw-Rectangle/Draw-Rectangle/Models/RectangleProperties.cs
@@ -12,10 +12,19 @@ namespace Draw_Rectangle.Models
         public string Length { get; set; }
         public string Width { get; set; }
 
-        public RectangleProperties()
-        {
+        //public RectangleProperties()
+        //{
 
-        }
+        //}
+
+        //internal void LoadPropertiesValue()
+        //{
+        //    var jsonFileInfo = Path.Combine(Directory.GetCurrentDirectory(), "RectangleValues.json");
+        //    dynamic jsonFile = JsonConvert.DeserializeObject(File.ReadAllText(jsonFileInfo));
+
+        //    Length = jsonFile.Length;
+        //    Width = jsonFile.Width;
+        //}
 
         internal void LoadPropertiesValue()
         {
@@ -24,6 +33,18 @@ namespace Draw_Rectangle.Models
 
             Length = jsonFile.Length;
             Width = jsonFile.Width;
+        }
+
+        internal void UpdateJsonFile(RectangleProperties rectangleValues)
+        {
+            string json = File.ReadAllText("RectangleValues.json");
+            dynamic jsonObj = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+
+            jsonObj["Length"] = rectangleValues.Length;
+            jsonObj["Width"] = rectangleValues.Width;
+
+            string output = Newtonsoft.Json.JsonConvert.SerializeObject(jsonObj, Newtonsoft.Json.Formatting.Indented);
+            File.WriteAllText("RectangleValues.json", output);
         }
     }
 }
